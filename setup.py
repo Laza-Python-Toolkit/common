d3977acbc4e24c0917f856182f25aa08f9d61228
David Kyalo <davidmkyalo@gmail.com>
2022-01-14 07:53:17 +0300

    Fix package loc

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 setup(
     name="laza-common",
-    version="0.0.1",
+    version="0.0.2",
     author="David Kyalo",
     description="A python development toolkit",
     long_description=Path("README.md").read_text(),
@@ -20,7 +20,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
-    packages=find_namespace_packages(where='src.*', include='laza.common'),
+    packages=find_namespace_packages(include='laza.common'),
     include_package_data=True,
     python_requires="~=3.9",
     install_requires=["typing-extensions ~=4.0.1"],
